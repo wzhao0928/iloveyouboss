0b799b3ab57f30c19340b1f7b377a7966d8bc0dc
wzhao0928 <wbzhao@thoughtworks.com>
2016-01-14 20:37:55 +0800

    test Profile::matches() throw exception when null criterion.getAnswer() return null. :pear: Libin & Wenbo

--- test/iloveyouboss/ProfileTest.java
+++ test/iloveyouboss/ProfileTest.java
@@ -1,6 +1,7 @@
 package iloveyouboss;
 
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by wbzhao on 1/14/16.
@@ -11,5 +12,16 @@ public class ProfileTest {
     public void matches_should_throw_exception_when_criteria_is_null() throws Exception {
         Profile profile = new Profile("test");
         profile.matches(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void matches_should_throw_exception_when_criterion_get_null_answer() throws Exception {
+        Profile profile = new Profile("test");
+        Criteria criteria = new Criteria();
+        Criterion mockCrit = mock(Criterion.class);
+        criteria.add(mockCrit);
+        when(mockCrit.getAnswer()).thenReturn(null);
+
+        profile.matches(criteria);
     }
 }
