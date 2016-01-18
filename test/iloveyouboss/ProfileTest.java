84b7854fc7133d7b74822440b125003b58a0034d
wzhao0928 <wbzhao@thoughtworks.com>
2016-01-18 23:36:35 +0800

    added two test case for criteria containing only one criterion, and extract duplicated setup statements into setup. :pear: Linbin & Wenbo

--- test/iloveyouboss/ProfileTest.java
+++ test/iloveyouboss/ProfileTest.java
@@ -5,17 +5,26 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ProfileTest {
 
-    Profile profile;
-    Criteria criteria;
+    private Profile profile;
+    private Criteria criteria;
+    private Criterion mockCrit1;
+    private Answer mockAnswer1;
 
     @Before
     public void setUp() throws Exception {
         profile = new Profile("test");
         criteria = new Criteria();
+        mockCrit1 = mock(Criterion.class);
+        mockAnswer1 = mock(Answer.class);
+
+        when(mockCrit1.getAnswer()).thenReturn(mockAnswer1);
+        when(mockAnswer1.getQuestionText()).thenReturn("mocked answer 1");
     }
 
     @Test(expected = NullPointerException.class)
@@ -25,9 +34,8 @@ public class ProfileTest {
 
     @Test(expected = NullPointerException.class)
     public void matches_should_throw_exception_when_criterion_get_null_answer() throws Exception {
-        Criterion mockCrit = mock(Criterion.class);
-        criteria.add(mockCrit);
-        when(mockCrit.getAnswer()).thenReturn(null);
+        criteria.add(mockCrit1);
+        when(mockCrit1.getAnswer()).thenReturn(null);
 
         profile.matches(criteria);
     }
@@ -40,16 +48,39 @@ public class ProfileTest {
 
     @Test
     public void matches_should_be_true_and_set_score_same_to_dont_care_when_the_only_criterion_dont_care_weight() throws Exception {
-        Criterion mockCrit = mock(Criterion.class);
-        Answer mockAnswer = mock(Answer.class);
-        when(mockCrit.getWeight()).thenReturn(Weight.DontCare);
-        when(mockAnswer.getQuestionText()).thenReturn("mocked answer");
-        when(mockCrit.getAnswer()).thenReturn(mockAnswer);
-        criteria.add(mockCrit);
+        when(mockCrit1.getWeight()).thenReturn(Weight.DontCare);
+        criteria.add(mockCrit1);
 
         boolean matched = profile.matches(criteria);
 
         assertThat(matched, equalTo(true));
         assertThat(profile.score(), equalTo(Weight.DontCare.getValue()));
     }
+
+    @Test
+    public void matches_should_be_true_and_set_score_same_to_criterion_weight_when_the_only_criterion_answer_matches() throws Exception {
+        when(mockAnswer1.match(any(Answer.class))).thenReturn(true);
+        when(mockCrit1.getWeight()).thenReturn(Weight.Important);
+        profile.add(mockAnswer1);
+        criteria.add(mockCrit1);
+
+        boolean matched = profile.matches(criteria);
+
+        assertThat(matched, equalTo(true));
+        assertThat(profile.score(), equalTo(Weight.Important.getValue()));
+    }
+
+    @Test
+    public void matches_should_be_false_and_set_score_to_0_when_the_only_criterion_answer_should_but_dont_match() throws Exception {
+        when(mockAnswer1.match(any(Answer.class))).thenReturn(false);
+        when(mockCrit1.getWeight()).thenReturn(Weight.MustMatch);
+        profile.add(mockAnswer1);
+        criteria.add(mockCrit1);
+
+        boolean matched = profile.matches(criteria);
+
+        assertThat(matched, equalTo(false));
+        assertThat(profile.score(), equalTo(0));
+    }
+
 }
