2f5d2639df72971f5b6b6e5c9f8c9d887f3f2188
wzhao0928 <wbzhao@thoughtworks.com>
2016-01-18 22:14:16 +0800

    extract setup and add test for criteria being empty. :pear: Libin & Wenbo

--- test/iloveyouboss/ProfileTest.java
+++ test/iloveyouboss/ProfileTest.java
@@ -1,27 +1,41 @@
 package iloveyouboss;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.*;
 
-/**
- * Created by wbzhao on 1/14/16.
- */
 public class ProfileTest {
+
+    Profile profile;
+
+    @Before
+    public void setUp() throws Exception {
+        profile = new Profile("test");
+    }
 
     @Test(expected = NullPointerException.class)
     public void matches_should_throw_exception_when_criteria_is_null() throws Exception {
-        Profile profile = new Profile("test");
         profile.matches(null);
     }
 
     @Test(expected = NullPointerException.class)
     public void matches_should_throw_exception_when_criterion_get_null_answer() throws Exception {
-        Profile profile = new Profile("test");
         Criteria criteria = new Criteria();
         Criterion mockCrit = mock(Criterion.class);
         criteria.add(mockCrit);
         when(mockCrit.getAnswer()).thenReturn(null);
 
         profile.matches(criteria);
+    }
+
+    @Test
+    public void matches_should_be_false_and_set_score_to_0_when_criteria_is_empty() throws Exception {
+        Criteria criteria = new Criteria();
+
+        assertThat(profile.matches(criteria), equalTo(false));
+        assertThat(profile.score(), equalTo(0));
     }
 }
