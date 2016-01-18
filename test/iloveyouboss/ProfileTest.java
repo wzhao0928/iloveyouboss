ccf7974bc4c2a5c144cb883c563321db3026c30c
wzhao0928 <wbzhao@thoughtworks.com>
2016-01-18 22:27:59 +0800

    add test for criteria containing only one criterion with don't-care-weight. :pear: Libin & Wenbo

--- test/iloveyouboss/ProfileTest.java
+++ test/iloveyouboss/ProfileTest.java
@@ -10,10 +10,12 @@ import static org.mockito.Mockito.*;
 public class ProfileTest {
 
     Profile profile;
+    Criteria criteria;
 
     @Before
     public void setUp() throws Exception {
         profile = new Profile("test");
+        criteria = new Criteria();
     }
 
     @Test(expected = NullPointerException.class)
@@ -23,7 +25,6 @@ public class ProfileTest {
 
     @Test(expected = NullPointerException.class)
     public void matches_should_throw_exception_when_criterion_get_null_answer() throws Exception {
-        Criteria criteria = new Criteria();
         Criterion mockCrit = mock(Criterion.class);
         criteria.add(mockCrit);
         when(mockCrit.getAnswer()).thenReturn(null);
@@ -33,9 +34,22 @@ public class ProfileTest {
 
     @Test
     public void matches_should_be_false_and_set_score_to_0_when_criteria_is_empty() throws Exception {
-        Criteria criteria = new Criteria();
-
         assertThat(profile.matches(criteria), equalTo(false));
         assertThat(profile.score(), equalTo(0));
+    }
+
+    @Test
+    public void matches_should_be_true_and_set_score_same_to_dont_care_when_the_only_criterion_dont_care_weight() throws Exception {
+        Criterion mockCrit = mock(Criterion.class);
+        Answer mockAnswer = mock(Answer.class);
+        when(mockCrit.getWeight()).thenReturn(Weight.DontCare);
+        when(mockAnswer.getQuestionText()).thenReturn("mocked answer");
+        when(mockCrit.getAnswer()).thenReturn(mockAnswer);
+        criteria.add(mockCrit);
+
+        boolean matched = profile.matches(criteria);
+
+        assertThat(matched, equalTo(true));
+        assertThat(profile.score(), equalTo(Weight.DontCare.getValue()));
     }
 }
